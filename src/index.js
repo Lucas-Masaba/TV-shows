7519b7b747d21f08c7a3cd29dd217b4ebc31939c
mre55 <misganawmihreteab>
2021-12-21 19:02:14 +0300

    Fix the suggestion to include the render header in to DOM content loader

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import './style.css';
 import UI from './UI.js';
 import fetchData from './display-list-of-shows.js';
 
-UI.headerLogo();
-
 document.addEventListener('DOMContentLoaded', async () => {
-  fetchData.fetchDataFromAPI();
+    UI.headerLogo();
+    fetchData.fetchDataFromAPI();
 });
