42efa235e8c512437c8fc1c17297f312951d1177
mre55 <misganawmihreteab>
2021-12-22 01:19:06 +0300

    Move the render method to the index.js file

--- src/display-list-of-shows.js
+++ src/display-list-of-shows.js
@@ -1,25 +1,16 @@
-const displayListOfShows = document.querySelector('.display-list-of-shows');
-
 // Unique identifier of the created app
 const appId = 'kUgCgfStndTaZOctty77';
 
-const fetchDataFromAPI = async () => {
+const fetchTVAPI = async () => {
   const TVResponse = await fetch('https://api.tvmaze.com/shows');
   const getShowResult = await TVResponse.json();
-
-  const showArray = getShowResult.slice(0, 6);
-
-  const involvementAPIResponse = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes`);
-  const getLikeResult = await involvementAPIResponse.json();
-
-  const values = showArray.map((result) => `<div class="display-show">
-    <img src="${result.image.medium}" alt="">
-    <p>${result.name}</p>
-    <p>${getLikeResult.filter((like) => parseInt((like.item_id), 10) === parseInt((result.id), 10))[0].likes} likes</p>
-    <button>Comments</button>
-    </div>`).join('');
-
-  displayListOfShows.innerHTML = values;
+  return getShowResult.slice(0, 6);
 };
 
-export default { fetchDataFromAPI };
+const fetchInvolvementAPI = async () => {
+  const involvementAPIResponse = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}/likes`);
+  const getLikeResult = await involvementAPIResponse.json();
+  return getLikeResult;
+};
+
+export default { fetchTVAPI, fetchInvolvementAPI };
